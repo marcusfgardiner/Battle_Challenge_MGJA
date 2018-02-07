d10a2a7db3984d515242af4053835c5694a4b266
jose <jennymarin1989@hotmail.com>
2018-02-07 15:08:56 +0000

    Text created for player name input-In progress

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    "Testing infrastructure working!"
+    erb :index
   end
 
   run! if app_file == $0
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,8 +1,19 @@
-require 'spec_helper'
+require 'spec_helper' #Fails if rspec/helper
 
 RSpec.feature "testing infraestructure", :type => :feature do
   scenario "testing infraestructure page" do
     visit "/"
-    expect(page).to have_text("Testing infrastructure working!")
+    expect(page).to have_text("Checking if is working")
+  end
+end
+
+RSpec.feature "player names", :type => :feature do
+  scenario "Enter player names" do
+    visit "/"
+    fill_in "player 1", with: "Marcus"
+    fill_2 "player 2", with: "Jenny"
+    click_button "Prepare to fight!"
+    expect(page).to have_content "Marcus\nJenny"
+
   end
 end
