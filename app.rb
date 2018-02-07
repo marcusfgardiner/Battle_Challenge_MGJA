4b5ac37157fb63f5a0859e3e59c25f692a46ae64
jose <jennymarin1989@hotmail.com>
2018-02-07 16:58:52 +0000

    Adding session in app.rb

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+ enable :sessions
+
   get '/' do
     erb :index
   end
@@ -11,6 +13,14 @@ class Battle < Sinatra::Base
   post '/names' do
     @player_1 = params[:name_1]
     @player_2 = params[:name_2]
+    session[:session_1]= @player_1
+    session[:session_2]= @player_2
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:session_1]
+    @player_2 = session[:session_2]
     erb :play
   end
 
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper' #Fails if rspec/helper
 
-RSpec.feature "testing infraestructure", :type => :feature do
-  scenario "testing infraestructure page" do
-    visit "/"
-    expect(page).to have_text("Checking if is working")
-  end
-end
+# RSpec.feature "testing infraestructure", :type => :feature do
+#   scenario "testing infraestructure page" do
+#     visit "/"
+#     expect(page).to have_text("Checking if is working")
+#   end
+# end
 
 RSpec.feature "player names", :type => :feature do
   scenario "Enter player names" do
@@ -13,7 +13,7 @@ RSpec.feature "player names", :type => :feature do
     fill_in "name_1", with: "Marcus"
     fill_in "name_2", with: "Jenny"
     click_button "PREEEPPPPARREEE FOR BATTTLLLEEE"
-    expect(page).to have_content "Marcus vs Jenny"
+    expect(page).to have_content "Marcus\nJenny"
 
   end
 end
